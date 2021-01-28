3003b9dacf8225a6003f3106419519c7b808c4a7
cora <cora@yunio.com>
2021-01-28 16:59:12 +0800

    init

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,28 +1,33 @@
 <template>
-  <div>
-    <a-form
-      name="custom-validation"
-      ref="ruleForm"
-      :model="ruleForm"
-      :rules="rules"
-      v-bind="{ labelCol: { span: 6 }, wrapperCol: { span: 18 } }"
-      @finish="handleFinish"
-      @finishFailed="handleFinishFailed"
-      class="login-form"
-    >
-      <h1>vue3 +ts</h1>
-      <a-form-item required has-feedback label="Name" name="name">
-        <a-input v-model:value="ruleForm.name" size="large" autocomplete="off" />
-      </a-form-item>
-      <a-form-item required has-feedback label="Password" name="password">
-        <a-input v-model:value="ruleForm.password" type="password" size="large" autocomplete="off" />
-      </a-form-item>
-      <a-form-item :wrapper-col="{ span: 24 }">
-        <a-button type="primary" html-type="submit" size="large" style="width: 100%;">
-          登录
-        </a-button>
-      </a-form-item>
-    </a-form>
+  <div class="user">
+		<div class="user-form">
+			<a-form
+				name="custom-validation"
+				ref="ruleForm"
+				:model="ruleForm"
+				:rules="rules"
+				v-bind="{ labelCol: { span: 6 }, wrapperCol: { span: 18 } }"
+				@finish="handleFinish"
+				@finishFailed="handleFinishFailed"
+				class="login-form"
+			>
+				<h1>vue3 +ts</h1>
+				<a-form-item required has-feedback label="Name" name="name">
+					<a-input v-model:value="ruleForm.name" size="large" autocomplete="off" />
+				</a-form-item>
+				<a-form-item required has-feedback label="Password" name="password">
+					<a-input v-model:value="ruleForm.password" type="password" size="large" autocomplete="off" />
+				</a-form-item>
+				<a-form-item :wrapper-col="{ span: 24 }">
+					<a-button type="primary" html-type="submit" size="large" style="width: 100%;">
+						登录
+					</a-button>
+				</a-form-item>
+			</a-form>
+		</div>
+		<canvas id="canvas">
+			抱歉，您的浏览器不支持canvas元素
+		</canvas>
   </div>
 </template>
 
@@ -66,17 +71,54 @@ export default defineComponent({
       } as User,
       rules: {}
     }
-  },
+	},
+	created() {
+		this.$nextTick(() => {
+			// this.initCanvas()
+		})
+	},
   methods: {
+		initCanvas () {
+			const canvas = document.createElement('canvas') as any
+			const postctx = (document.getElementById('canvas') as any).getContext('2d')
+			const ctx = canvas.getContext('2d')
+			const ctxCanvas = ctx.canvas
+			console.log('ctxCanvas:　', canvas.height, canvas.width, ctxCanvas.width)
+			ctx.fillStyle = 'hsl(200deg, 100%, 2%)'
+			ctx.fillRect(0, 0, canvas.width, canvas.height)
+			ctx.save()
+			ctx.translate(canvas.width, canvas.height) // 重新开始绘制
+			ctx.beginPath() // 重新开始一条路径
+			ctx.restore() // 返回之前保存过的路径状态和属性
+
+			postctx.drawImage(ctxCanvas, 0, 0)
+		}
   }
 })
 </script>
 <style lang="less" scoped>
+.user{
+	height: 100%;
+	width: 100%;
+	position: relative;
+	.user-form {
+		padding-top: 200px;
+	}
   .login-form {
-    width: 460px;
-    margin: 200px auto;
+		width: 460px;
+    margin: auto;
     border: 1px solid #ccc;
     padding: 48px;
-    box-shadow: 3px 3px 30px #ccc;
-  }	
+		box-shadow: 3px 3px 30px #ccc;
+		background-color: #fff;
+	}
+	#canvas {
+		position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+	}
+}
 </style>
