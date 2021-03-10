fa31ec44ccdddb2a18582b03ade1118e0996cfe2
cora <cora@yunio.com>
2021-03-10 15:48:26 +0800

    添加eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,24 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
-  rules: {
+	rules: {
+		'@typescript-eslint/ban-ts-ignore': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: '^h$',
+				varsIgnorePattern: '^h$'
+			}
+		],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'generator-star-spacing': 'off',
